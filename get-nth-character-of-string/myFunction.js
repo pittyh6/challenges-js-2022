e378728766ba1f45e55b5da99090cc9df761c649
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-04-17 15:59:07 +1000

    done challenge with success

--- get-nth-character-of-string/myFunction.js
+++ get-nth-character-of-string/myFunction.js
@@ -1,6 +1,15 @@
 // Write a function that takes a string (a) and a number (n) as argument
 // Return the nth character of 'a'
 
-function myFunction(a,n){
-    
+//starts from 1
+function myFunctionOne(a,n){
+    console.log(a[n-1])
 }
+
+myFunctionOne("bsubasuf", 2);
+
+//starts from 0
+function myFunctionTwo(a,n){
+    console.log(a[n])
+}
+myFunctionTwo("bsubasuf", 2);
