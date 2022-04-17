a72857e4cb054f6f232b16418f8b2221c45bd731
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-04-17 16:23:52 +1000

    chllenge done get-last-n-characters-of-string [wdb-689]

---
+++ get-last-n-characters-of-string/myFunction.js
@@ -0,0 +1,9 @@
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+
+function myFunction(str){
+    console.log(str.substring(str.length-3)) 
+}
+
+myFunction("PriscilaFe")
