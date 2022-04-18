1309364eb5277982ba9b4c4c3d6c0102c41f2b7a
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-04-18 10:25:39 +1000

    check-if-a-number-is-even [wdb-695] done

---
+++ check-if-a-number-is-even/myFuction.js
@@ -0,0 +1,17 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+function myFunction(a)
+{
+    // if(a%2 ===0){
+    //     console.log("true")
+    // }else{
+    //     console.log("false")
+    // }
+    console.log(a%2===0)
+}
+
+myFunction(10)
+myFunction(-4)
+myFunction(5)
+myFunction(-111)
