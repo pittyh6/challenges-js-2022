bb3a8978148f0fafa5e316f79b48fd303427aac2
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-04-24 11:08:07 +1000

    done - clear-up-the-chaos-behind-these-strings - [wdb-702]

--- Basic/clear-up-the-chaos-behind-these-strings/myFunction.js
+++ Basic/clear-up-the-chaos-behind-these-strings/myFunction.js
@@ -4,6 +4,32 @@
 // 'Javascript', 'Countryside', and 'Downtown'
 // You might want to apply basic JS string methods such as replace(), split(), slice() etc
 function myFunction(a, b){
-    
-    return 
+    let cleanA = ''
+    let cleanB = ''
+    let upperCaseA = ''
+    let reverseB = ''
+
+    if(a.includes('%') || b.includes('%')){
+        cleanA = a.replace('%', '');
+        cleanB = b.replace('%', '');
+
+        upperCaseA = cleanA.charAt(0).toUpperCase() + cleanA.slice(1)
+
+        reverseB = cleanB.split("").reverse().join("");
+
+        console.log(upperCaseA + reverseB);
+    }
+    return (upperCaseA + reverseB)
 }
+
+myFunction('java', 'tpi%rcs')
+myFunction('c%ountry', 'edis')
+myFunction('down', 'nw%ot')
+
+
+function myFunction2(a, b) {
+    const func = x => x.replace('%','');
+    const first = func(a);
+    const second = func(b).split('').reverse().join('');
+    return first.charAt(0).toUpperCase() + first.slice(1) + second;
+  }
