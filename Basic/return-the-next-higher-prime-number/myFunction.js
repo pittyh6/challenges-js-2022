bda5ddb0e231e25d8d7b5ecb4a80ac7e7793c1d8
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-05-05 17:37:51 +1000

    find prime number [wdb-703]

--- Basic/return-the-next-higher-prime-number/myFunction.js
+++ Basic/return-the-next-higher-prime-number/myFunction.js
@@ -3,10 +3,32 @@
 // If a is prime, return a
 // If not, return the next higher prime number
 function myFunction(a){
-   
+  console.log("AAAA == " + a)
+  if(a<2){
+    console.log("FALSE = a<2 "+ a)
+    return false;
+  }else if(a >1 && a<4){
+    console.log("TRUE = a >1 && a<4 "+ a)
+    return a;
+  }
+
+  let root = Math.ceil(Math.sqrt(a))
+  console.log("root = " + root)
+  for(let i = 2; i <=root; i++){
+    if(a % i === 0){
+      console.log("for if -> false " + i + " a= " +a)
+      return false;
+    }
+  }
+  
+  console.log("TRUEEEEE")
+  return a;  
+
 }
 
 myFunction(38)
 myFunction(7)
 myFunction(115)
 myFunction(2000)
+myFunction(2)
+myFunction(121)
