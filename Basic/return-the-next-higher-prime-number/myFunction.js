216c15c3761b8a6530a2b4f56cacd799851b5990
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-05-06 15:30:19 +1000

    done next prime number. [wdb-703]

--- Basic/return-the-next-higher-prime-number/myFunction.js
+++ Basic/return-the-next-higher-prime-number/myFunction.js
@@ -1,34 +1,42 @@
-// This challenge is a little bit more complex
 // Write a function that takes a number (a) as argument
-// If a is prime, return a
+// This challenge is a little bit more complex
 // If not, return the next higher prime number
-function myFunction(a){
-  console.log("AAAA == " + a)
-  if(a<2){
-    console.log("FALSE = a<2 "+ a)
-    return false;
-  }else if(a >1 && a<4){
-    console.log("TRUE = a >1 && a<4 "+ a)
-    return a;
-  }
+// If a is prime, return a
 
+function myFunction(a){
+  let resultCount = false;  
+  if(a<2){    
+    for(let count = a+1; resultCount !== true; count++){
+      myFunction(count)
+      resultCount = true
+      a = count
+      break
+    }
+  }else if(a >=2 && a<4){
+    console.log("value 2 = " + a)
+    return a;
+    
+  }  
   let root = Math.ceil(Math.sqrt(a))
-  console.log("root = " + root)
   for(let i = 2; i <=root; i++){
     if(a % i === 0){
-      console.log("for if -> false " + i + " a= " +a)
-      return false;
+      for(let count = a+1; resultCount !== true; count++){
+        myFunction(count)
+        resultCount = true
+        return count
+      }
     }
   }
   
   console.log("TRUEEEEE")
+  console.log("A final= " + a)
   return a;  
-
 }
-
+myFunction(1)
 myFunction(38)
 myFunction(7)
 myFunction(115)
 myFunction(2000)
 myFunction(2)
-myFunction(121)
+myFunction(50)
+myFunction(938)
