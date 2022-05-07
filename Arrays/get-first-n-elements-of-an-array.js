5468c5cfeec9169d85ecdd32222474753efd5b3e
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-05-07 11:25:04 +1000

    done -  get-first-n-elements-of-an-array [wds-5]- splice()

---
+++ Arrays/get-first-n-elements-of-an-array.js
@@ -0,0 +1,16 @@
+// Write a function that takes an array (a) as argument
+// Extract the first 3 elements of a
+// Return the resulting array
+function myFunction(a){
+    console.log(a.splice(0,3));
+    return a.splice(0,3)
+}
+
+myFunction([1,2,3,4])
+//Expected[1,2,3]
+myFunction([5,4,3,2,1,0])
+//Expected[5,4,3]
+myFunction([99,1,1])
+//Expected[99,1,1]
+myFunction([99,1,1,4,3,2,1,0])
+//Expected[99,1,1]
