7fe7e1afe505d053f701f14bb37fdfbd7b7960f7
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-05-08 16:32:20 +1000

    done - merge-an-arbitrary-number-of-arrays - [wds-15] - used ARGUMENTS

--- Arrays/merge-an-arbitrary-number-of-arrays.js
+++ Arrays/merge-an-arbitrary-number-of-arrays.js
@@ -13,6 +13,10 @@ function myFunction(...arrays){
     return result
 }
 
+//arguments is an Array-like object accessible inside functions that contains the values of the arguments passed to that function.
+//The arguments object is useful for functions called with more arguments than they are formally declared to accept. This technique is useful for functions that can be passed a variable number of arguments, such as Math.min(). 
+//###You can use arguments.length to count how many arguments the function was called with. If you instead want to count how many parameters a function is declared to accept, inspect that function's length property.
+
 myFunction([1, 2, 3], [4, 5, 6])
 //Expected [1, 2, 3, 4, 5, 6]
 myFunction(['a', 'b', 'c'], [4, 5, 6])
