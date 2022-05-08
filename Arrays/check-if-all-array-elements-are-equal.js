e69e5efc6ac87380d4fb3ab5935197de41f31dc1
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-05-08 14:31:20 +1000

    done - check-if-all-array-elements-are-equal - [wds-14] - used  EVERY()

---
+++ Arrays/check-if-all-array-elements-are-equal.js
@@ -0,0 +1,45 @@
+// Write a function that takes an array as argument
+// It should return true if all elements in the array are equal
+// It should return false otherwise
+function myFunction1(arr){
+    let result = true
+    for(let i=0; i< arr.length-1; i++){
+        if(arr[i] === arr[i+1]){
+            result = true
+        }else{
+            result = false
+            break
+        }
+    }
+    console.log("result = " + result)
+    return result
+}
+
+function myFunction2(arr){
+    let result = true
+    for(let i=0; i< arr.length-1; i++){
+        if(arr[i] !== arr[i+1]){
+            result = false
+            break
+        }else{
+            result = true            
+        }
+    }
+    console.log("result = " + result)
+    return result
+}
+
+function myFunction(arr){
+    let result = (currentValue) => currentValue === arr[0]
+    console.log( "result = " + arr.every(result))
+    return arr.every(result)
+}
+
+myFunction([true, true, true, true])
+//Expected true 
+myFunction(['test', 'test', 'test'])
+// //Expected true 
+myFunction([1,1,1,2])
+// //Expected false 
+myFunction(['10',10,10,10])
+// //Expected false 
